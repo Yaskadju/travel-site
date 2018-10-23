f403784c6e95bc46b7ae7e81893b5bf6208d3396
Thales <thales.y@gmail.com>
2018-10-22 23:20:40 -0300

    creating reusable blocks

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -18,10 +18,19 @@ gulp.task('watch', function () {
 	// ** means future hypotetical folder and *.css means any file with this extension created in the future
 	watch('./app/assets/styles/**/*.css', function () {
 		gulp.start('cssInject');
+		// any save that we make, triggers this cssInject class
+		// this will make the dependencies ['styles] begin and complete
+		// before running the cssInject task
+		// so that way, the compiled style.css file will have a chance to 
+		// be generated, and only on that point, we will pipe it in to the browsersync
 	})
 });
 
 gulp.task('cssInject', ['styles'], function () {
+	// ['styles'] tells gulp to begin and complete any dependency task
+	// inside [], which is this case is 'styles', that is a dependency of the cssInject task 
 	return gulp.src('./app/temp/styles/styles.css')
 		.pipe(browserSync.stream());
 });
+
+// always add "return" in an assync function
